d1ec4cf2248e29cae7971611e0b4698426d29787
Jakob <Jakob@DESKTOP-RSUVK13>
2020-10-03 11:27:08 -0700

    Initial version of the assignment.

---
+++ assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,36 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	
+	//Private int that calculates the total
+	private int total;
+	
+	
+	//Method that adds numbers
+	public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	//Method that returns the total
+	public int getTotal () {
+	    return 0;
+	  }
+	 
+	//Method that adds
+	public void add (int value) {
+	  }
+	
+	//Method that subtracts
+	public void subtract (int value) {
+	  }
+	
+	//Returns the operation as a string
+	public String toString () {
+	    return "";
+	  }
+	
+	//Clears the calculator
+	public void clear() {
+	  }
+
+}
