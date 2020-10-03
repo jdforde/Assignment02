6bc7438b075923e4c11282985aaa52e3d09ad068
Jakob <Jakob@DESKTOP-RSUVK13>
2020-10-03 11:54:00 -0700

    Second version of assignment

--- assignment02/src/cse360assignment02/AddingMachine.java
+++ assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,36 +1,47 @@
 package cse360assignment02;
 
+
+/*This class acts as a machine that adds and subtracts numbers. It gets the total and stores the history, 
+ *like a calculator, in a string. */
 public class AddingMachine {
 	
-	//Private int that calculates the total
+	//Private int that calculates the total and String that shows history
 	private int total;
+	private String history;
 	
 	
-	//Method that adds numbers
+	//Constructor method
 	public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = "0";
 	  }
 	  
 	//Method that returns the total
 	public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	 
 	//Method that adds
 	public void add (int value) {
+		total+=value;
+		history+=" + " + value;
 	  }
 	
 	//Method that subtracts
 	public void subtract (int value) {
+		total-=value;
+		history+=" - " + value;
 	  }
 	
 	//Returns the operation as a string
 	public String toString () {
-	    return "";
+	    return history;
 	  }
 	
-	//Clears the calculator
+	//Clears the calculator to default position
 	public void clear() {
+		total = 0;
+		history = "0";
 	  }
 
 }
--- assignment02/src/cse360assignment02/Main.java
+++ assignment02/src/cse360assignment02/Main.java
@@ -3,8 +3,16 @@ package cse360assignment02;
 public class Main {
 
 	public static void main(String[] args) {
+		AddingMachine myCalc = new AddingMachine();
+		myCalc.add(4);
+		myCalc.subtract(2);
+		myCalc.add(5);
+		System.out.println(myCalc.getTotal());
+		System.out.println(myCalc.toString());
+		myCalc.clear();
+		myCalc.add(21);
+		System.out.println(myCalc.toString());
 		
-
 	}
 
 }
